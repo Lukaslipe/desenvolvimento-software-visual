b779f185683ddadd2dbe7838117974dfb18d7ed9
lukaslipe <lucascristodev@gmail.com>
2025-08-18 21:46:10 -0300

    Feat: Criação de rotas

--- Ecommerce/API/Program.cs
+++ Ecommerce/API/Program.cs
@@ -1,6 +1,14 @@
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
-app.MapGet("/", () => "Hello World!");
+// Funcionalidade - Requisições
+// - URL | Caminho | Endereço
+// - Um método HTTP
+
+app.MapGet("/", () => "Grupo pinho");
+
+app.MapGet("/pinho", () => "É a melhor empresa do mundo!!");
+
+app.MapPost("/pinholog", () => "Funcionalidade do POST");
 
 app.Run();
