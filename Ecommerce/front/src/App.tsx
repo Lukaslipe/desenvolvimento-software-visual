f4f911fb4917a3f219731c9789a336a5b3d40fdb
lukaslipe <lucascristodev@gmail.com>
2025-10-27 21:27:52 -0300

    Fazendo requisição

--- Ecommerce/front/src/App.tsx
+++ Ecommerce/front/src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import ListarProdutos from './ListarProdutos';
 
 function App() {
   return (
     <div id="app">
       <h1>Minha primeira aplicação em React</h1>
+      <ListarProdutos/>
     </div>
   );
 }
